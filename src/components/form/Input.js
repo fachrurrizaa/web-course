2780ad56f02cdc7304b0507c4db40364eef10681
Muhammad Fachrur Riza <mfriza69@gmail.com>
2023-03-31 20:52:39 +0700

    Create Sign In Page

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Input({ label, placeholder }) {
+export default function Input({ label, placeholder, type }) {
   return (
     <div className="form-control w-full max-w-xs">
         <label className="label">
             <span className="label-text font-normal text-[#160442] text-base mb-3">{ label }</span>
         </label>
-        <input type="text" placeholder={ placeholder } className="input input-bordered w-full max-w-xs text-[#6B7193] rounded-full mb-4" />
+        <input type={ type } placeholder={ placeholder } className="input input-bordered w-full max-w-xs text-[#6B7193] rounded-full mb-4" />
     </div>
   )
 }
--- src/components/form/SignIn.js
+++ src/components/form/SignIn.js
@@ -7,8 +7,8 @@ export default function SignIn() {
     <div>
         <div className='flex justify-center items-center flex-col'>
             <h1 className='font-bold text-5xl text-[#160442] mb-16 mt-28'>Welcome Back</h1>
-            <Input label={"Email Address"} placeholder={"Type your email"}/>
-            <Input label={"Password"} placeholder={"Type your password"}/>
+            <Input type={"email"} label={"Email Address"} placeholder={"Type your email"}/>
+            <Input type={"password"} label={"Password"} placeholder={"Type your password"}/>
             <Button  className={'btn max-w-xs w-full text-white bg-[#642DFB] mt-8 font-semibold outline-none border-none hover:bg-[#642dfbbe] rounded-full'} content={"Sign In"}/>
             <Button className={'btn max-w-xs w-full text-[#160442] bg-[#EBEDF3] mt-4 font-medium outline-none border-none hover:bg-slate-300 rounded-full'} content={"Create New Account"}/>
         </div>
