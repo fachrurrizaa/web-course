b72419a875f84e1536584b648433f412bb77ce0d
21.11.4318_Muhammad Fachrur Riza <mfriza69@gmail.com>
2023-06-24 14:58:41 +0700

    resolve conflict

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,19 +8,8 @@ export default function page() {
   return (
     <>
         <Navbar/>
-        <div className="flex flex-col items-center">
-            <div className="mr-[140px]">
-                <h1 className="font-normal text-2xl text-[#160442] leading-[63px] text-center mt-16 mb-3">The Story Behind Project Web Course</h1>
-            </div>
-            <div className="flex mb-24 mt-16 gap-20 mr-32">
-               
-            </div>
-            <div class="flex gap-32">
-            </div>
-        </div>
-        <Footer/>
         <About/>
-        
+        <Footer/>
     </>
   )
 }
