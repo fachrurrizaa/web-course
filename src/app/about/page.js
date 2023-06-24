9caa4aad4c09a29e2bdec27e1293f6260d3fcf8a
4326_Gusti Ayu Putu Febriyanti <gustiayu@students.amikom.ac.id>
2023-06-24 14:23:14 +0700

    update menu about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,6 @@
 import About from '@/components/About'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/navbar/Navbar'
-
 import React from 'react'
 
 export default function page() {
