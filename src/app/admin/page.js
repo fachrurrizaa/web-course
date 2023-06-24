a9cc387286171d9fe51fde5ae33ecce0533cbdca
4326_Gusti Ayu Putu Febriyanti <gustiayu@students.amikom.ac.id>
2023-06-24 23:32:09 +0700

    tampilan menu admin sementara

---
+++ src/app/admin/page.js
@@ -0,0 +1,17 @@
+import Admin from 'src/components/Admin.js'
+import Material from '@/components/Material'
+
+
+import React from 'react'
+
+export default function page() {
+  return (
+
+    <>
+    <Admin />
+    <Material/>
+   
+    </>
+
+  )
+}
---
+++ src/components/Material.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Material() {
+  return (
+    <><div className="card w-96 bg-base-100 shadow-xl image-full">
+          <figure><img src="/public/assets/analisis data.png" alt="Shoes" /></figure>
+          <div className="card-body">
+              <h2 className="card-title">Belajar Analisis Data dengan Phyton</h2>
+              <p>Ingin tau bagaimana cara menganalisis data dengan phyton? ayo ikuti course Analisis Data</p>
+              <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Upload</button>
+              </div>
+          </div>
+      </div><div className="card w-60 shadow-xl image-full">
+              <figure><img src="/public/assets/analisis data.png" alt="Shoes" /></figure>
+              <div className="card-body">
+                  <h2 className="card-title">Belajar Analisis Data dengan Phyton</h2>
+                  <p>Ingin tau bagaimana cara menganalisis data dengan phyton? ayo ikuti course Analisis Data</p>
+                  <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Upload</button>
+                  </div>
+              </div>
+          </div></>
+  )
+}
