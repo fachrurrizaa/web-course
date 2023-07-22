382ba95bcfbe83cb87589a4350ebea7c94be092f
21.11.4318_Muhammad Fachrur Riza <mfriza69@gmail.com>
2023-07-22 23:14:08 +0700

    arghh 1

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -5,7 +5,7 @@ import { useSession } from 'next-auth/react';
 export default function page() {
     const {data : session} = useSession();
 
-    if(!session){
+    if(session === null){
         return  (
             <h1>Wait bro</h1>
         )
@@ -13,10 +13,10 @@ export default function page() {
     return (
         <Layout>
             <div className='text-blue-900 flex justify-between overflow-hidden'>
-                <h2>Hello, {session.user.name}</h2>
+                <h2>Hello, {session.user?.name}</h2>
                 <div className='flex bg-gray-300 gap-1 text-black rounded-lg'>
-                    <img src={session.user.image} alt="" className='w-6 h-6'/>
-                    <span className='px-2'>{session.user.name}</span>
+                    <img src={session.user?.image} alt="" className='w-6 h-6'/>
+                    <span className='px-2'>{session.user?.name}</span>
                 </div>
             </div>
         </Layout>
