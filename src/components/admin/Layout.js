5a28591cb0f05ff3aaf9f5b2de3498e294c80d94
21.11.4318_Muhammad Fachrur Riza <mfriza69@gmail.com>
2023-07-23 00:39:27 +0700

    fix

--- src/components/admin/Layout.js
+++ src/components/admin/Layout.js
@@ -13,18 +13,19 @@ export default function page({children}) {
             <h1>Wait bro</h1>
         )
     }
-    if ((session?.user?.email !== email) && (session?.user?.email !== email1)){
-        return (
-            <ForbidenAccess/>
-        )
-    }
-    if ((session?.user?.email === email) || (session?.user?.email === email1)){
-        return (
-            <div className='min-h-screen bg-teal-50 flex'>
-                <NavAdmin/>{
-                <div className='bg-teal-50 flex-grow m-3 rounded-lg p-4'>{children}</div>
-                }
-            </div>
-        )
+    if(session){
+        if ((session?.user?.email !== email) && (session?.user?.email !== email1)){
+            return (
+                <ForbidenAccess/>
+            )
+        }
+        else if ((session?.user?.email === email) || (session?.user?.email === email1)){
+            return (
+                <div className='min-h-screen bg-teal-50 flex'>
+                    <NavAdmin/>
+                    <div className='bg-teal-50 flex-grow m-3 rounded-lg p-4'>{children}</div>
+                </div>
+            )
+        }
     }
 }
