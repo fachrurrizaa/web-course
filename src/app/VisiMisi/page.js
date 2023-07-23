bf88a0376eca1961bb4e06cb7a86a25166ef6879
laee57390.gmail.com <you@example.comgit config --global user.email>
2023-07-23 11:26:08 +0700

    penambahan pada sejarah perusahaan

--- src/app/VisiMisi/page.js
+++ src/app/VisiMisi/page.js
@@ -26,7 +26,7 @@ export default function Page() {
           <div className='w-1/2 mx-auto'>
             <h2 className='text-xl font-semibold'>Misi:</h2>
             <p className='text-justify'>
-              Menyediakan akses yang mudah,fleksibel dan terjangkau dengan materi design online yang berkualitas tinggi untuk pengguna web design guna meningkatkan keterampilan design,mengembangkan karier,hingga menambah portofolio para designer
+              Menyediakan akses yang mudah,fleksibel dan terjangkau dengan materi design online yang berkualitas tinggi untuk pengguna web design untuk meningkatkan keterampilan design,mengembangkan karier,hingga menambah portofolio para designer
             </p>
           </div>
         </div>
@@ -34,3 +34,4 @@ export default function Page() {
     </div>
   );
 }
+ 
